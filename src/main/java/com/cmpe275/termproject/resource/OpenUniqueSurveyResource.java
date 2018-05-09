308fe0ae5ab9724e0315752c71d2e9b124661c61
sriharshav1 <sriharsha.vanga@sjsu.edu>
2018-05-09 01:08:02 -0700

    email with attachment

--- src/main/java/com/cmpe275/termproject/resource/OpenUniqueSurveyResource.java
+++ src/main/java/com/cmpe275/termproject/resource/OpenUniqueSurveyResource.java
@@ -20,7 +20,41 @@ import javax.mail.internet.MimeMessage;
 import java.util.Map;
 import java.util.Properties;
 import java.util.UUID;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import java.util.HashMap;
+import java.io.File;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import  java.util.Date;
 
+import java.io.IOException;
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping(value = "/survey/openUniqueSurvey/emailRegister")
@@ -86,61 +120,173 @@ public class OpenUniqueSurveyResource {
             return new ResponseEntity("http://localhost:3000/survey/"+current_survey.getInt("surveyId")+"/"+String.valueOf(uuid), HttpStatus.OK);
         }
 
-        else{
+        else {
 
-        SurveyEntity surveyEntity = surveyRepository.findOne(current_survey.getInt("surveyId"));
-        OpenUniqueSurveyEntity openUniqueSurveyEntity = new OpenUniqueSurveyEntity();
+            SurveyEntity surveyEntity = surveyRepository.findOne(current_survey.getInt("surveyId"));
+            OpenUniqueSurveyEntity openUniqueSurveyEntity = new OpenUniqueSurveyEntity();
 
-        openUniqueSurveyEntity.setSurveyId(surveyEntity);
-        openUniqueSurveyEntity.setUuid(String.valueOf(uuid));
-        openUniqueSurveyEntity.setEmailId(jsonObject.getString("usreid"));
-        openUniqueSurveyEntity.setInvitation_link("http://localhost:8080/" + surveyEntity.getSurveyId() + "/" + String.valueOf(uuid));
-        openUniqueSurveyEntity.setIslinkused(0);
+            openUniqueSurveyEntity.setSurveyId(surveyEntity);
+            openUniqueSurveyEntity.setUuid(String.valueOf(uuid));
+            openUniqueSurveyEntity.setEmailId(jsonObject.getString("usreid"));
+            openUniqueSurveyEntity.setInvitation_link("http://localhost:8080/" + surveyEntity.getSurveyId() + "/" + String.valueOf(uuid));
+            openUniqueSurveyEntity.setIslinkused(0);
 
-            if(openUniqueSurveyRepository.findOneByEmailIdAndSurveyId(jsonObject.getString("usreid"),surveyEntity) != null )
-            {
-                OpenUniqueSurveyEntity op = openUniqueSurveyRepository.findOneByEmailIdAndSurveyId(jsonObject.getString("usreid"),surveyEntity);
-                return new ResponseEntity("http://localhost:3000/survey/"+current_survey.getInt("surveyId")+"/"+op.getUuid(), HttpStatus.OK);
+            if (openUniqueSurveyRepository.findOneByEmailIdAndSurveyId(jsonObject.getString("usreid"), surveyEntity) != null) {
+                OpenUniqueSurveyEntity op = openUniqueSurveyRepository.findOneByEmailIdAndSurveyId(jsonObject.getString("usreid"), surveyEntity);
+                return new ResponseEntity("http://localhost:3000/survey/" + current_survey.getInt("surveyId") + "/" + op.getUuid(), HttpStatus.OK);
             }
 
-        openUniqueSurveyRepository.save(openUniqueSurveyEntity);
+            openUniqueSurveyRepository.save(openUniqueSurveyEntity);
 
-        // Send Email
+            // Send Email
 
-        final String username = "infosurveyape275@gmail.com";
-        final String password = "qwerty123asdf";
 
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
+            try {
+                String qrCodeData = "http://localhost:3000/survey/" + current_survey.getInt("surveyId") + "/" + String.valueOf(uuid);
+                //String filePath = "C:\\Users\\sriha\\Desktop\\SurveyApp\\google.png";
+                String filePath = "src/main/resources/InvitationCode.png";
+                String charset = "UTF-8"; // or "ISO-8859-1"
+                Map<EncodeHintType, ErrorCorrectionLevel> hintMap = new HashMap<EncodeHintType, ErrorCorrectionLevel>();
+                hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+                BitMatrix matrix = new MultiFormatWriter().encode(
+                        new String(qrCodeData.getBytes(charset), charset),
+                        BarcodeFormat.QR_CODE, 200, 200, hintMap);
+                MatrixToImageWriter.writeToFile(matrix, filePath.substring(filePath
+                        .lastIndexOf('.') + 1), new File(filePath));
+                System.out.println("QR Code image created successfully!");
+            } catch (Exception e) {
+                System.err.println(e);
+            }
 
-        Session session = Session.getInstance(props,
-                new javax.mail.Authenticator() {
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(username, password);
-                    }
-                });
 
-        try {
+            try {
+                String host = "smtp.gmail.com";
+                String port = "587";
+                String mailFrom = "infosurveyape275@gmail.com";
+                String password = "qwerty123asdf";
 
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("infosurveyape275@gmail.com"));
-            message.setRecipients(Message.RecipientType.TO,
-                    InternetAddress.parse(jsonObject.getString("usreid")));
-            message.setSubject("Please verify your email address");
-            message.setText( "Hi "+jsonObject.getString("usreid")+ ","+  System.lineSeparator() +"Thank you for registering for the survey!"+ System.lineSeparator() +"Follow this Link to take the survey : "+System.lineSeparator()+ "http://localhost:3000/survey/"+current_survey.getInt("surveyId")+"/"+String.valueOf(uuid));
+                // message info
+                String mailTo = (jsonObject.getString("usreid"));
+                String subject = "You are invited to the survey";
+                String message="Hi "+jsonObject.getString("usreid")+ ","+  System.lineSeparator() +"Thank you for registering for the survey!"+ System.lineSeparator() +"Follow this Link to take the survey : "+System.lineSeparator()+ "http://localhost:3000/survey/"+current_survey.getInt("surveyId")+"/"+String.valueOf(uuid);
 
-            Transport.send(message);
+                // attachments
+                String[] attachFiles = new String[1];
+                attachFiles[0] = "src/main/resources/InvitationCode.png";
+
+                sendEmailWithAttachments(host, port, mailFrom, password, mailTo,
+                        subject, message, attachFiles);
+                System.out.println("Email sent.");
+            } catch (Exception ex) {
+                System.out.println("Could not send email.");
+                ex.printStackTrace();
+            }
+
+
+//        Session session = Session.getInstance(props,
+//                new javax.mail.Authenticator() {
+//                    protected PasswordAuthentication getPasswordAuthentication() {
+//                        return new PasswordAuthentication(username, password);
+//                    }
+//                });
+//
+//        try {
+//
+//            Message message = new MimeMessage(session);
+//            message.setFrom(new InternetAddress("infosurveyape275@gmail.com"));
+//            message.setRecipients(Message.RecipientType.TO,
+//                    InternetAddress.parse(jsonObject.getString("usreid")));
+//            message.setSubject("Access your survey here");
+//            String MessageContentCustom="Hi "+jsonObject.getString("usreid")+ ","+  System.lineSeparator() +"Thank you for registering for the survey!"+ System.lineSeparator() +"Follow this Link to take the survey : "+System.lineSeparator()+ "http://localhost:3000/survey/"+current_survey.getInt("surveyId")+"/"+String.valueOf(uuid);
+//            message.setText(MessageContentCustom );
+
+//            MimeBodyPart messageBodyPart = new MimeBodyPart();
+//
+//                Multipart multipart = new MimeMultipart();
+//
+//                messageBodyPart = new MimeBodyPart();
+//                messageBodyPart.setContent(MessageContentCustom, "text/html");
+//                String file = "src/main/resources/InvitationCode.png";
+//                String fileName = "attachmentName";
+//                DataSource source = new FileDataSource(file);
+//                messageBodyPart.setDataHandler(new DataHandler(source));
+//                messageBodyPart.setFileName("fileName.jpg");
+//                messageBodyPart.setHeader("Content-ID", "<image>");
+//                multipart.addBodyPart(messageBodyPart);
+//
+//                message.setContent(multipart);
+//           // message.setText( "Hi "+jsonObject.getString("usreid")+ ","+  System.lineSeparator() +"Thank you for registering for the survey!"+ System.lineSeparator() +"Follow this Link to take the survey : "+System.lineSeparator()+ "http://localhost:3000/survey/"+current_survey.getInt("surveyId")+"/"+String.valueOf(uuid));
+//
+//            Transport.send(message);
 
             System.out.println("Done");
+        }
 
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        }
-        }
 
         return new ResponseEntity("OK", HttpStatus.OK);
     }
+
+
+    public static void sendEmailWithAttachments(String host, String port,
+                                                final String userName, final String password, String toAddress,
+                                                String subject, String message, String[] attachFiles)
+            throws AddressException, MessagingException {
+        // sets SMTP server properties
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", port);
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.user", userName);
+        properties.put("mail.password", password);
+
+        // creates a new session with an authenticator
+        Authenticator auth = new Authenticator() {
+            public PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(userName, password);
+            }
+        };
+        Session session = Session.getInstance(properties, auth);
+
+        // creates a new e-mail message
+        Message msg = new MimeMessage(session);
+
+        msg.setFrom(new InternetAddress(userName));
+        InternetAddress[] toAddresses = { new InternetAddress(toAddress) };
+        msg.setRecipients(Message.RecipientType.TO, toAddresses);
+        msg.setSubject(subject);
+        msg.setSentDate(new Date());
+
+        // creates message part
+        MimeBodyPart messageBodyPart = new MimeBodyPart();
+        messageBodyPart.setContent(message, "text/html");
+
+        // creates multi-part
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(messageBodyPart);
+
+        // adds attachments
+        if (attachFiles != null && attachFiles.length > 0) {
+            for (String filePath : attachFiles) {
+                MimeBodyPart attachPart = new MimeBodyPart();
+
+                try {
+                    attachPart.attachFile(filePath);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+
+                multipart.addBodyPart(attachPart);
+            }
+        }
+
+        // sets the multi-part as e-mail's content
+        msg.setContent(multipart);
+
+        // sends the e-mail
+        Transport.send(msg);
+
+    }
+
+
 }
