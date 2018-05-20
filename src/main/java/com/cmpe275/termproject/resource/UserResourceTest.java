85d6a0b3d8279b960858bdde9f2916024e4fbd07
Divyank Shukla <divyank68@yahoo.co.in>
2018-05-20 12:17:23 -0700

    setting up test resource

---
+++ src/main/java/com/cmpe275/termproject/resource/UserResourceTest.java
@@ -0,0 +1,16 @@
+package com.cmpe275.termproject.resource;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import org.json.JSONObject;
+import org.junit.*;
+import org.junit.Assert.*;
+
+//import static org.junit.*;
+
+
+public class UserResourceTest {
+
+    
+}
