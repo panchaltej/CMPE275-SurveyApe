1d6fa6db22033a4a46e615562c930ef1e35e9c75
Divyank Shukla <divyank68@yahoo.co.in>
2018-05-20 12:54:29 -0700

    Removing extra dependencies

--- src/main/java/com/cmpe275/termproject/resource/UserResourceTest.java
+++ src/main/java/com/cmpe275/termproject/resource/UserResourceTest.java
@@ -5,9 +5,6 @@ import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import org.json.JSONObject;
 import org.junit.*;
-import org.junit.Assert.*;
-
-//import static org.junit.*;
 
 
 public class UserResourceTest {
